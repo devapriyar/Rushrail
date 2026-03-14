1fcea11adc83df01df3e250b5216394be0fbf952
anjal192 <anjalkrishnapv123@gmail.com>
2026-03-14 14:24:05 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,70 @@
+ 
+const { addUserToQueue, getQueueLength } = require("./queue");
+const express = require("express");
+const cors = require("cors");
+const db = require("./firebase");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("Tatkal Backend Running");
+});
+app.post("/bookTicket", async (req,res)=>{
+
+  const data = req.body;
+
+  await db.collection("bookings").add(data);
+
+  res.json({
+    message: "Ticket booked successfully"
+  });
+
+});
+app.post("/cancelTicket", async (req,res)=>{
+
+  const bookingId = req.body.bookingId;
+
+  await db.collection("bookings").doc(bookingId).delete();
+
+  res.json({
+    message: "Ticket cancelled"
+  });
+
+});
+app.post("/joinQueue", async (req,res)=>{
+
+  const userId = req.body.userId;
+
+  await addUserToQueue(userId);
+
+  const position = await getQueueLength();
+
+  res.json({
+    message: "Joined Queue",
+    queue_position: position
+  });
+
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
+app.post("/joinQueue", async (req,res)=>{
+
+ const userId = "user_" + Date.now();
+
+ await addUserToQueue(userId);
+
+ const position = await getQueueLength();
+
+ res.send({
+   message: "Joined Queue",
+   user: userId,
+   queue_position: position
+ });
+
+});
