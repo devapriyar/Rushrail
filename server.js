6066fdc1f2ab91f0090a57807292daf5a4d912f6
anjal192 <anjalkrishnapv123@gmail.com>
2026-03-14 10:10:21 +0530

    backend files upload

---
+++ server.js
@@ -0,0 +1,32 @@
+ 
+const { addUserToQueue, getQueueLength } = require("./queue");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Tatkal Backend Running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
+app.post("/joinQueue", async (req,res)=>{
+
+ const userId = "user_" + Date.now();
+
+ await addUserToQueue(userId);
+
+ const position = await getQueueLength();
+
+ res.send({
+   message: "Joined Queue",
+   user: userId,
+   queue_position: position
+ });
+
+});
