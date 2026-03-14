abd8f09c7fc0f78c278c17900440548060ab6515
Devapriya R <devapriyarajesh6282@gmail.com>
2026-03-14 13:11:49 +0530

    frontend files upload

---
+++ js/app.js
@@ -0,0 +1,23 @@
+function goToDashboard(){
+window.location.href="dashboard.html"
+}
+
+function goToSearch(){
+window.location.href="search.html"
+}
+
+function goToBooking(){
+window.location.href="booking.html"
+}
+
+function goToPayment(){
+window.location.href="payment.html"
+}
+
+function goToCancel(){
+window.location.href="cancel.html"
+}
+
+function goToHome(){
+window.location.href="index.html"
+}
