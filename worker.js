bc3f44b381313f1f879fa8480b4163c08a4d5827
abhinave2010 <abhinave2010@gmail.com>
2026-03-14 15:32:50 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -15,13 +15,50 @@ async function startWorker() {
 
     try {
 
-      for (let i = 0; i < 5; i++) {   // process 5 users at once
+      const queueLength = await client.lLen("tatkal_queue");
+      console.log("Checking queue... Current length:", queueLength);
+
+      for (let i = 0; i < 5; i++) {
 
         const user = await client.rPop("tatkal_queue");
 
-        if (user) {
-          console.log("Processing user:", user);
+        if (!user) {
+          console.log("No users in queue");
+          break;
         }
+
+        console.log("Processing user:", user);
+
+        // check seats
+        const seats = parseInt(await client.get("seats_available_12627"));
+
+        if (seats > 0) {
+
+          await client.decr("seats_available_12627");
+
+          console.log("Seat booked for:", user);
+          console.log("Remaining seats:", seats - 1);
+           await db.collection("bookings").add({
+
+    userId: user,
+
+    train: "12627",
+
+    status: "CONFIRMED",
+
+    time: new Date()
+
+  });
+
+        } else {
+
+          console.log("No seats available. Added to waitlist:", user);
+          await client.lPush("waitlist", user);
+
+        }
+
+
+        await new Promise(resolve => setTimeout(resolve, 2000));
 
       }
 
@@ -29,7 +66,7 @@ async function startWorker() {
       console.log("Worker error:", err);
     }
 
-  }, 3000); // runs every 3 seconds
+  }, 3000);
 
 }
 
